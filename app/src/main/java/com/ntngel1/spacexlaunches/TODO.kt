8a23bfa8ba7748bece83467f7e9025bba02a20f4
Kirill <kirill@webant.ru>
2019-11-18 13:48:33 +0300

    Chain для карточки ресурса

--- app/src/main/java/com/ntngel1/spacexlaunches/TODO.kt
+++ app/src/main/java/com/ntngel1/spacexlaunches/TODO.kt
@@ -1,10 +1,9 @@
 package com.ntngel1.spacexlaunches
 
-// TODO flightNumber -> FlightNumber
+// TODO launchId -> FlightNumber
 // TODO BasePresenter, Fragment
 // TODO Gateway -> Repository
 // TODO ID Naming
-// TODO FullscreenImagesDialogFragment Refactoring
 // TODO compositeDisposable += ........ instead of .let(compositeDisposable::add)
 // TODO Locale datetime
 // TODO Base RecyclerView.Adapter?
