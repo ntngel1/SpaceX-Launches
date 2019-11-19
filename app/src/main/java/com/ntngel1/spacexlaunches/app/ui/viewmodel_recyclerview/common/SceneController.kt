bb2455f9ddc92f88060bbf80c824f13c191fce92
Kirill <kirill@webant.ru>
2019-11-19 11:03:53 +0300

    Добавлены TODO

--- app/src/main/java/com/ntngel1/spacexlaunches/app/ui/viewmodel_recyclerview/common/SceneController.kt
+++ app/src/main/java/com/ntngel1/spacexlaunches/app/ui/viewmodel_recyclerview/common/SceneController.kt
@@ -2,6 +2,8 @@ package com.ntngel1.spacexlaunches.app.ui.viewmodel_recyclerview.common
 
 import androidx.collection.ArrayMap
 
+// TODO Separate class for ViewStateStore impl
+// TODO Injecting ViewStateStore via dagger subcomponent with some scope
 abstract class SceneController : ViewStateStore {
 
     abstract fun buildViewModels(): List<ViewModel>
